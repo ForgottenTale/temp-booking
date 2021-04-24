2eabe7b5628a5725d3ae0725268b0551674d8702
Gautham P R <gauthampr18@gmail.com>
2021-04-24 18:04:49 +0530

    Removed cors

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const routes = require('./services/routes/index.js');
 const auth = require('./services/auth.js');
 var cors = require('cors');
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials:true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials:true
+// }));
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
