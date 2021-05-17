2820aa63450855cfbbb0345a19bd4e3d3d313905
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-05-18 03:27:13 +0530

    update to request view and settings

--- src/components/topNav/topNav.js
+++ src/components/topNav/topNav.js
@@ -81,6 +81,7 @@ export default function TopNav({ activeComponent, setUser, setOU, ou, user,setAu
     return (
         <div className="topNav">
             <div className="topNav_title">
+                <div className="topNav_title_con">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -96,35 +97,13 @@ export default function TopNav({ activeComponent, setUser, setOU, ou, user,setAu
                     <rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect>
                 </svg>
                 <h3>{activeComponent}</h3>
+                </div>
+                <button onClick={
+                    () => handlelogOut()
+
+                }>Logout</button>
             </div>
             <div className="topNav_icons">
-                {/* <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="feather feather-bell"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 01-3.46 0"></path>
-                        </svg>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="feather feather-settings"
-                            viewBox="0 0 24 24"
-                        >
-                            <circle cx="12" cy="12" r="3"></circle>
-                            <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z"></path>
-                        </svg> */}
                 <div className="topNav_select">
                     <Select
                         value={selectedOU}
